f3393aba527323269b05e5c3e1fb849c1b777eb7
MaTor97 <mttortora97@gmail.com>
2024-10-16 10:17:46 +0200

    Create a server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const PORT = 4500
+const app =  express()
+
+app.get('/', (req, res) => {
+    res.send({
+        info:`Hello World!`
+    })
+})
+
+
+app.listen(PORT, () => console.log(`Server for lokkeroom started: http://localhost:${PORT}/`))
