62410ec75609c8740c2fa5e48073576524129c8c
MaTor97 <mttortora97@gmail.com>
2024-10-17 15:41:36 +0200

    Serveur fonctionnel et requète pour /api/register

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import sql from "./dbConfig.js";
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/api/register', async (req, res) => {
+    try {
+        const { username, password, email } = req.body;
+        await sql`
+            INSERT INTO users (username, password, email)
+            VALUES (${username}, ${password}, ${email})
+            RETURNING *;
+        `;
+        console.log('Done');
+        res.status(201).json('The user has been created').end();
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+app.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`Server listening on http://localhost:${PORT}`);
+});
