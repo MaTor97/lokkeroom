5ae1dc8eaa677fcab7792d1ea5f32ad444d12191
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2024-10-18 15:25:33 +0200

    Add userRoutes.js for user-related endpoints

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser'
 import auth from './authRoutes.js';
 import lobby from './lobbyRoutes.js'
 import message from './messageRoutes.js'
+import users from './userRoutes.js';
 
 const app = express();
 const PORT = 3000;
@@ -10,9 +11,10 @@ const PORT = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', auth)
-app.use('/', lobby)
-app.use('/', message)
+app.use('/', auth);
+app.use('/', lobby);
+app.use('/', message);
+app.use('/', users);
 
 
 
